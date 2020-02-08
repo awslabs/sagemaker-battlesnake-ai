803ec44e644e0aac0c4ea176e9b0e503456328ab
Xavier Raffin <xraffin@amazon.com>
2020-02-07 16:40:01 -0800

    Working APIGW

--- lambda.py
+++ lambda.py
@@ -5,5 +5,16 @@ print('Loading function')
 
 def start(event, context):
     #print("Received event: " + json.dumps(event, indent=2))
-    return "started!"  # Echo back the first key value
+    return {
+        "statusCode": 200,
+        "body": json.dumps('Snake started')
+    }
+    #raise Exception('Something went wrong')
+
+def move(event, context):
+    #print("Received event: " + json.dumps(event, indent=2))
+    return {
+        "statusCode": 200,
+        "body": json.dumps('Snake moved')
+    }
     #raise Exception('Something went wrong')
