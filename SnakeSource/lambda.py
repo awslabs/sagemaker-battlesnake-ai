4e63c70e7bea016d82e156fe9235fb19965e9730
Xavier Raffin <xraffin@amazon.com>
2020-02-07 17:36:20 -0800

    Tentative real snake

---
+++ SnakeSource/lambda.py
@@ -0,0 +1,87 @@
+import json
+import os
+import random
+import time
+import mxnet as mx
+import numpy as np
+
+from convert_utils import ObservationToStateConverter
+
+#################################
+#              Init             #
+#################################
+
+converter = ObservationToStateConverter(style='one_versus_all', use_border=True)
+net = mx.gluon.SymbolBlock.imports('model-symbol.json', 'data', 'model-0000.params')
+
+def ping():
+    return {
+        "statusCode": 200
+    }
+
+def start():
+    print("Start")
+    color = "#00FF00"
+    time.sleep(0.1)
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": { "color": color }
+    }
+
+def move(data):
+    print("Move")
+    print(data)
+
+    state = converter.get_game_state(data)
+    
+    # Get the list of possible directions
+    i,j = np.unravel_index(np.argmax(state[:,:,1], axis=None), state[:,:,1].shape)
+    snakes = state[:,:,1:].sum(axis=2)
+    possible = []
+    if snakes[i+1,j] == 0:
+        possible.append('down')
+    if snakes[i-1,j] == 0:
+        possible.append('up')
+    if snakes[i,j+1] == 0:
+        possible.append('right')
+    if snakes[i,j-1] == 0:
+        possible.append('left')
+
+    # Sending the states for inference
+    state_nd = mx.nd.array(state)
+    state_nd = state_nd.expand_dims(axis=0).transpose((0, 3, 1, 2))
+    
+    # Getting the result from the model
+    output = net(state_nd)
+    
+    # Getting the highest predicted index
+    direction_index = output.argmax(axis=1)[0].asscalar()
+    
+    directions = ['up', 'down', 'left', 'right']
+    direction = directions[int(direction_index)]
+    choice = direction
+    if direction in possible:
+        choice = direction
+    elif len(possible) > 0:
+        choice = random.choice(possible)
+    else:
+        choice = direction
+
+    print("Choice" + choice)
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": { "move": choice }
+    }
+
+
+def end():
+    return {
+        "statusCode": 200
+    }
