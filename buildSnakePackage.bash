ba3f89eff40f68e803f3c5cb0209d547b6eab817
Xavier Raffin <xraffin@amazon.com>
2020-02-19 11:23:51 -0800

    Automate lambda packaging

--- buildSnakePackage.bash
+++ buildSnakePackage.bash
@@ -1,1 +1,29 @@
 # This script build the snake Lambda package
+#
+# prerequisite: python 2.7, virtualenv, pip
+
+echo " "
+echo " > Start packaging model on Lambda"
+echo " "
+
+# Create a virtualenv python 2.7
+virtualenv venv
+source venv/bin/activate
+
+# Install mxnet package
+pip install mxnet
+
+# Copy model into the package directory and zip it into a lambda package
+cp -r PreTrainedSnake/SnakeModelEndpoint/* venv/lib/python2.7/site-packages
+cd venv/lib/python2.7/site-packages
+zip -9r ../../../../model-lambda-package.zip .
+
+# Deactivate virtualenv
+deactivate
+
+# Cleanup
+rm -rf venv
+
+echo " "
+echo " > Your Lambda package model-lambda-package.zip is ready"
+echo " "
