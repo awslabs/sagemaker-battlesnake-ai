0845e7044da966714988580f54846439c0444e68
Xavier Raffin <xraffin@amazon.com>
2020-02-20 08:38:21 -0800

    Now update package works

--- updateDeploymentPackage.bash
+++ updateDeploymentPackage.bash
@@ -45,8 +45,8 @@ displayEndExecCmd () {
   echo "$2"
   echo
   eval cmd="$1"
-  eval ${cmd}
   echo ${cmd}
+  eval ${cmd}
   echo
 }
 
@@ -58,14 +58,14 @@ updateOrCreateLayer () {
     COMMAND="aws s3 cp mxnet-layer-package.zip s3://$S3_PREFIX$1/$LAYER_PACKAGE_SOURCE_KEY --region $1 $AWS_PROFILE"
     displayEndExecCmd \${COMMAND} " > Copy MXNet lambda layer package source to region "$1
 
-    COMMAND="aws lambda delete-layer-version --layer-name $LAYER_NAME --version-number 1 --region $1 $AWS_PROFILE"
-    displayEndExecCmd \${COMMAND} " > Delete Layer $LAYER_NAME in region "$1
-
     LAYER_DESCRIPTION="MXnet layer for $PYTHON_VERSION require SciPy layer"
-    COMMAND="aws lambda publish-layer-version --layer-name $LAYER_NAME --description \"$LAYER_DESCRIPTION\" --license-info \"MIT\" --content S3Bucket=$S3_PREFIX$1,S3Key=$LAYER_PACKAGE_SOURCE_KEY --compatible-runtimes $PYTHON_VERSION --region $1 $AWS_PROFILE"
+    COMMAND="aws lambda publish-layer-version --layer-name $LAYER_NAME --description \"$LAYER_DESCRIPTION\" --license-info \"MIT\" --content S3Bucket=$S3_PREFIX$1,S3Key=$LAYER_PACKAGE_SOURCE_KEY --compatible-runtimes $PYTHON_VERSION --region $1 $AWS_PROFILE > cmddump.txt"
     displayEndExecCmd \${COMMAND} " > Create Lambda layer in region "$1
 
-    COMMAND="aws lambda add-layer-version-permission --layer-name $LAYER_NAME --statement-id public --action lambda:GetLayerVersion  --principal \"*\" --version-number 1 --output text --region $1 $AWS_PROFILE"
+    LAYER_VERSION=`cat cmddump.txt | grep "Version\":" | sed 's/[^0-9]*//g'`
+    echo "Layer Version is $LAYER_VERSION"
+
+    COMMAND="aws lambda add-layer-version-permission --layer-name $LAYER_NAME --statement-id public --action lambda:GetLayerVersion  --principal \"*\" --version-number $LAYER_VERSION --output text --region $1 $AWS_PROFILE"
     displayEndExecCmd \${COMMAND} " > Making Layer Version public in region "$1
 }
 
