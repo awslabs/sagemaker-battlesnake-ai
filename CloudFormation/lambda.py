6a19efbafda283875b060d145f59023b61a91380
Xavier Raffin <xraffin@amazon.com>
2020-02-27 08:48:51 -0800

    Start lambda deploy function

---
+++ CloudFormation/lambda.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+ # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ # 
+ # Licensed under the Apache License, Version 2.0 (the "License").
+ # You may not use this file except in compliance with the License.
+ # A copy of the License is located at
+ # 
+ #     http://www.apache.org/licenses/LICENSE-2.0
+ # 
+ # or in the "license" file accompanying this file. This file is distributed 
+ # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+ # express or implied. See the License for the specific language governing 
+ # permissions and limitations under the License.
+
+import json
+import os
+import boto3
+from botocore.vendored import requests
+
+
+def lambda_handler(event, context):
+    try:
+        operation = event['ResourceProperties']['Operation']
+
+        if operation == 'CleanupSagemakerBucket':
+            if event['RequestType'] == 'Delete':
+                bucket = os.environ['SAGEMAKER_BUCKET_ARN']
+                s3 = boto3.resource('s3')
+                bucket = s3.Bucket(bucket)
+                for obj in bucket.objects.filter():
+                    s3.Object(bucket.name, obj.key).delete()
+
+        sendResponseCfn(event, context, "SUCCESS")
+    except Exception as e:
+        print(e)
+        sendResponseCfn(event, context, "FAILED")
+
+
+def sendResponseCfn(event, context, responseStatus):
+    response_body = {'Status': responseStatus,
+                     'Reason': 'Log stream name: ' + context.log_stream_name,
+                     'PhysicalResourceId': context.log_stream_name,
+                     'StackId': event['StackId'],
+                     'RequestId': event['RequestId'],
+                     'LogicalResourceId': event['LogicalResourceId'],
+                     'Data': json.loads("{}")}
+
+    requests.put(event['ResponseURL'], data=json.dumps(response_body))
