fb2253748f02c10770116d6d8d6044646b628edd
Xavier Raffin <xraffin@amazon.com>
2020-03-03 14:35:39 -0800

    Fix lambda imports after code move

--- InferenceEndpoint/SnakeInference/lambda.py
+++ InferenceEndpoint/SnakeInference/lambda.py
@@ -15,12 +15,10 @@ import json
 import os
 import random
 import time
-import mxnet as mx
 import numpy as np
 import boto3
 import botocore
 
-from battlesnake_heuristics import MyBattlesnakeHeuristics
 from convert_utils import ObservationToStateConverter
 
 #################################
