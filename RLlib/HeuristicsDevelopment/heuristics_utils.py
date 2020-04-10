9bd11be9abdb15c0774064cd00e6f6c499bd0a49
Jonathan <jono.chung@gmail.com>
2020-04-10 04:39:37 +0000

    RLlib heuristics

--- RLlib/HeuristicsDevelopment/heuristics_utils.py
+++ RLlib/HeuristicsDevelopment/heuristics_utils.py
@@ -1,9 +1,6 @@
-from mxnet_src.networks.utils import sort_states_for_snake_id
 import numpy as np
-import mxnet as mx
-from collections import namedtuple
-
-ctx = mx.gpu() if mx.context.num_gpus() > 0 else mx.cpu()
+import tensorflow as tf
+from rllib_src.utils import sort_states_for_snake_id
 
 def remove_borders_from_state(state):
     '''
@@ -32,6 +29,17 @@ def convert_food_maxtrix_to_list(in_array):
     for x_, y_ in zip(x, y):
         food.append({"x": x_, "y": y_})
     return food
+
+def build_state_for_snake(obs, snake_i, prev_state=None):    
+    if prev_state is None:
+        prev_state = np.zeros((obs.shape[0], obs.shape[1], 3))
+        
+    obs = np.array(obs, dtype=np.float32)
+            
+    obs = sort_states_for_snake_id(obs, snake_i+1)
+    merged_map = np.concatenate((prev_state, obs), axis=-1)
+
+    return merged_map
 
 def make_snake_lists(env):
     '''
@@ -88,83 +96,19 @@ def convert_state_into_json(state, snake_list, snake_id, turn_count, health):
         
     return json
 
-def get_action(net, state, snake_id, turn_count, health, memory):
-    '''
-    Processes the input data to be fed into the defined neural network.
+def get_action(net, state):   
+    state = np.expand_dims(state, 0)
+    predict = net(observations=tf.convert_to_tensor(state, dtype=tf.float32), 
+               seq_lens=tf.constant([-1], dtype=tf.int32), 
+               prev_action=tf.constant([-1], dtype=tf.int64),
+               prev_reward=tf.constant([-1], dtype=tf.float32), 
+               is_training=tf.constant(False, dtype=tf.bool))
+    action = predict["actions"].numpy()[0]
+    return action
 
-    Parameters:
-    -----------
-    `net`: gluon.sequential
-    model of the neural net
-    
-    `state`: np.array of size (3, map_size[0]+2, map_size[1]+2)
-    Provides the current observation of the gym
-    
-    `snake_id`: int
-    Indicates the id where id \in [0...number_of_snakes]
-    
-    `turn_count`: int
-    Indicates the number of elapsed turns
-    
-    `health`: dict
-    Indicates the health of all snakes in the form of {snake_id: health}
-    
-    `memory`: (state, turn_count, health)
-    Indicates the state, turn_count, and health of the previous turn.
-    
-    Returns:
-    -----------
-    `action`: np.array 
-    The expected q value of the action.
-    i.e., the larger the value the better the action is.
-    To get the next best action, perform np.argmax(action)
-    '''
-    
-    sequence_length = 2
-    state_i = sort_states_for_snake_id(state, snake_id+1, one_versus_all=True)
-    previous_state_i = sort_states_for_snake_id(memory.state, snake_id+1, one_versus_all=True)
-    state_sequence = mx.nd.array(np.stack([previous_state_i, state_i]), ctx=ctx).transpose((0, 3, 1, 2)).expand_dims(0)
-    
-    snake_id_sequence = mx.nd.array([snake_id]*sequence_length, ctx=ctx).expand_dims(0)
-    turn_count_sequence = mx.nd.array([memory.turn_count, turn_count], ctx=ctx).expand_dims(0)
-    snake_health_sequence = mx.nd.array([memory.health[snake_id], health[snake_id]], ctx=ctx).expand_dims(0)
-        
-    action = net(state_sequence, snake_id_sequence, turn_count_sequence, snake_health_sequence)
-    '''
-    Net takes the following arguments:
-    `net(state, snake_id, turn_count, snake_health)`
+def simulate(env, net, heuristics, number_of_snakes):    
+    state, _, _, infos  = env.reset()
 
-    `state`: *nd.array* of size (batch_size, sequence_length=2, c=3, map_size[0]+2, mapsize[1]+2)
-    - Provides the observation space of the gym
-    - `batch_size` should be set to 1
-    - `sequence_length` provides the number of timesteps back the model considers. Give `t` is the current time step, `c=0`   refers to `t-1` and `c=1` is t.
-    - Each `c` slide refers to the *food*, *current snake*, and *other snakes* respectively.
-    - `map_size` is based on the size of the BattlesnakeGym +2 for the -1 border.
-
-    `snake_id`: *nd.array* of size (batch_size, sequence_length=2)
-    - Provides the id of the snake, which is a i \in [0...number_of_snakes-1]
-
-    `turn_count`: *nd.array* of size (batch_size, sequence_length=2)
-    - Provides the number of turns that has elapsed (obtained from `info["current_turn"]` in the gym)
-
-    `snake_health`: *nd.array* of size (batch_size, sequence_length=2)
-    - Provides the health of the snake (obtained from `info["snake_health"]` in the gym)
-    '''
-    return action.asnumpy()[0]
-
-def simulate(env, net, heuristics, number_of_snakes):
-    '''
-    Helper functions to simulate the snakes moving with BattlesnakeGym.
-    Pseudo code is:
-    Until only 1 snake is alive:
-        1. Get actions from the neural network
-        2. Run heuristisc
-        3. Feed into the battlesnake gym
-    '''
-    Memory = namedtuple("Memory", "state turn_count health")
-    
-    state, _, _, infos = env.reset()
-    
     rgb_arrays = [env.render(mode="rgb_array")]
     infos_array = [infos]
     actions_array = [[4, 4, 4, 4]]
@@ -172,17 +116,22 @@ def simulate(env, net, heuristics, number_of_snakes):
         
     heuristics_log_array = [{k: "" for k in range(number_of_snakes)}]
 
-    memory = Memory(state=np.zeros(state.shape), turn_count=infos["current_turn"], health=infos["snake_health"])
+    previous_state = {}
+    for i in range(number_of_snakes):
+        agent_id = "agent_{}".format(i)
+        previous_state[agent_id] = None
+        
     while True:
         infos["current_turn"] += 1
 
-        heuristics_log = {}
+        heuristics_log = {}       
         actions = []
         for i in range(number_of_snakes):
-            action = get_action(net, state, snake_id=i,
-                                turn_count=infos["current_turn"]+1,
-                                health=infos["snake_health"],
-                                memory=memory)
+            agent_id = "agent_{}".format(i)
+            
+            state_i = build_state_for_snake(state, i, previous_state[agent_id])
+                        
+            action = get_action(net, state)
 
             snake_list = make_snake_lists(env)
             json = convert_state_into_json(state, snake_list, snake_id=i, 
@@ -198,10 +147,9 @@ def simulate(env, net, heuristics, number_of_snakes):
             heuristics_log[i] = heuristics_log_string
             
             actions.append(action)
-        memory = Memory(state=state, turn_count=infos["current_turn"], 
-                        health=infos["snake_health"])
+            previous_state[agent_id] = state_i
         
-        next_state, reward, dones, infos = env.step(np.array(actions))
+        next_state, reward, dones, infos = env.step(actions)
         
         rgb_array = env.render(mode="rgb_array")
         rgb_arrays.append(rgb_array.copy())
